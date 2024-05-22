280560ea3d71843122460a6183077d4ec2adb2b0
Eddie Sandler <ed.sandler@gmail.com>
2024-05-22 12:13:38 -0400

    styled login/reg screens

--- health/frontend/src/components/Auth.jsx
+++ health/frontend/src/components/Auth.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { Container, Typography, Link } from '@mui/material';
 import RegistrationForm from './RegistrationForm';
 import LoginForm from './LoginForm';
+import './Auth.css'; // Import the CSS file
 
 const Auth = ({ isRegister, toggleForm, handleLogin }) => {
   return (
-    <Container>
-      {isRegister ? (
-        <RegistrationForm />
-      ) : (
-        <LoginForm handleLogin={handleLogin} />
-      )}
-      <Typography variant="body2" align="center" style={{ marginTop: '10px' }}>
-        {isRegister ? 'Already have an account?' : "Don't have an account?"}{' '}
-        <Link component="button" onClick={toggleForm} style={{ cursor: 'pointer' }}>
-          {isRegister ? 'Login' : 'Register'}
-        </Link>
-      </Typography>
-    </Container>
+    <div className="authBackground">
+      <Container className="authContainer">
+        {isRegister ? (
+          <RegistrationForm />
+        ) : (
+          <LoginForm handleLogin={handleLogin} />
+        )}
+        <Typography variant="body2" align="center" style={{ marginTop: '10px' }}>
+          {isRegister ? 'Already have an account?' : "Don't have an account?"}{' '}
+          <Link component="button" onClick={toggleForm} style={{ cursor: 'pointer' }}>
+            {isRegister ? 'Login' : 'Register'}
+          </Link>
+        </Typography>
+      </Container>
+    </div>
   );
 };
 
