f8ffc55d987a73b8059e005cf895491d6d816b2a
Eddie Sandler <ed.sandler@gmail.com>
2024-05-29 09:26:20 -0400

    updated Navbar

--- health/frontend/src/components/NavBar.jsx
+++ health/frontend/src/components/NavBar.jsx
@@ -15,12 +15,13 @@ const NavBar = ({ isLoggedIn, handleLogout, showLoginForm, currentPath }) => {
       <Link to="/" className="navLink" style={{ pointerEvents: currentPath === '/compare' ? 'none' : 'auto', color: currentPath === '/compare' ? 'gray' : 'inherit' }}>
         Home
       </Link>
+
+      <div className="navRight">
       {isLoggedIn && (
         <Link to="/update-profile" className="navLink">
           Update Profile
         </Link>
       )}
-      <div className="navRight">
         {isLoggedIn ? (
           <button onClick={logout} className="toggleButton">
             Log Out
